b67fff170c8d1ed88cd115e92f77f7124dfd2cca
Ibrahim Shenoud <eprahimshenouda88@gmail.com>
2023-03-16 07:24:53 -0700

    edit code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-        char str[] = "and that piece of art is useful\"\ - Dora Korpar, 2015-10-19\n";
+        char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
         puts(str);
         return (0);
