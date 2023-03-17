d7f163890d330bb6ab46a734275d50c7892293bc
Ibrahim Shenoud <eprahimshenouda88@gmail.com>
2023-03-16 17:51:45 -0700

    last edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - Entry point
  *
@@ -9,6 +10,8 @@ int main(void)
 	const char str[] =
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str, sizeof(str) - 1);
+	size_t str_len = strlen(str);
+
+	fwrite(str, sizeof(char), str_len, stderr);
 	return (1);
 }
