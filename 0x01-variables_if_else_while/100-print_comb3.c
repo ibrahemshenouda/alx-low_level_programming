e97860975df932d0273781aa32816c5e99702b23
Ibrahim Shenoud <eprahimshenouda88@gmail.com>
2023-03-20 18:57:06 -0700

    first advanced task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48, j;
+
+	for (i; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (!(i == 56 && j == 57))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
