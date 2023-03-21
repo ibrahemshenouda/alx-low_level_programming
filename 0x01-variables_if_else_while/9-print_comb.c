bbb2d25888d3b18680af9752ba1a7dd781cec638
Ibrahim Shenoud <eprahimshenouda88@gmail.com>
2023-03-20 18:04:17 -0700

    task 10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x < 58)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar(10);
+	return (0);
+}
