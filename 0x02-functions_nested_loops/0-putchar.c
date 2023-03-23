0fcd19be50a5a463e0a069db3eea1a9084bf07ff
Ibrahim Shenoud <eprahimshenouda88@gmail.com>
2023-03-23 16:44:05 -0700

    first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void print(void);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print();
+	return (0);
+}
+
+/**
+ * print - void function
+ *
+ * void function print _putchar word
+ */
+void print(void)
+{
+	printf("_putchar\n");
+}
